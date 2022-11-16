855cbc68c167a552aa637d16d7a9cb76d667e590
17Frazer_O <17Frazer_O@bexleygs.co.uk>
2022-11-16 10:12:22 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {22, 13, 10, 80, 19, 54, 51, 81, 30, 99, 91, 73, 65, 36, 35, 74, 82, 12, 43, 6};
+
+    }
+}
---
+++ src/com/company/SortedNumbers.java
@@ -0,0 +1,23 @@
+package com.company;
+
+class SortedNumber{
+
+    public static void main(String[] args) {
+        int[] numbers = {22, 13, 10, 80, 19, 54, 51, 81, 30, 99, 91, 73, 65, 36, 35, 74, 82, 12, 43, 6};
+
+        for(int i=0;i < numbers.length-1; i++){
+    int min=i;
+    for(int j = i+1 ;j < numbers.length; j++){
+
+        if(numbers[j] < numbers[min]);
+        min=j;
+    }
+
+}}}
+
+
+public class SortedNumbers {
+    public String numbers(int i){
+        return numbers(0);
+}}
+
